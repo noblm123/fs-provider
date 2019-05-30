9e4624aa0a8b367ebd1da766143de9ed2779bc27
Mark Noble <marknoble@Marks-MacBook-Pro.local>
2019-05-30 01:57:47 +0100

    commit

--- src/app/new-rental/new-rental.page.ts
+++ src/app/new-rental/new-rental.page.ts
@@ -10,7 +10,21 @@ export class NewRentalPage {
 
   constructor(private navCtrl: NavController) { }
 
-   
+  navToNewRental() {
+    this.navCtrl.navigateForward("new-rental");
+  }
+  navToRentalDetails() {
+    this.navCtrl.navigateForward("rental-details");
+  }
+  navToUpdateRental() {
+    this.navCtrl.navigateForward("update-rental");
+  }
+  navToExistingRentals() {
+    this.navCtrl.navigateForward("existing-rentals");
+  }
+
+
+}
   
 
 }
